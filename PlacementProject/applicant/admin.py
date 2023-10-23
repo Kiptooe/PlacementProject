8e063300efd4d86beb44a5d7586283ba9e4122da
Emmanuel-Kip <84289692+Emmanuel-Kip@users.noreply.github.com>
2023-10-23 18:51:46 +0300

    Added models

--- PlacementProject/applicant/admin.py
+++ PlacementProject/applicant/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from .models import Applicant, MinistryType, Ministry
+from .models import Applicant, MinistryType, Ministry, Institution, Course, Cluster, ClusterSubject, ClusterSubjectCategory, Grade, Basket, Payment
+from django_otp.admin import OTPAdminSite
+from django.contrib.auth.models import User
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
 
 
-admin.site.register(Applicant)
-admin.site.register(Ministry)
-admin.site.register(MinistryType)
+class OTPAdmin(OTPAdminSite):
+    pass
+
+
+admin_site = OTPAdmin(name='OTPAdmin')
+admin_site.register(User)
+admin_site.register(TOTPDevice, TOTPDeviceAdmin)
+admin_site.register(Applicant)
+admin_site.register(MinistryType)
+admin_site.register(Ministry)
+admin_site.register(Institution)
+admin_site.register(Course)
+admin_site.register(Cluster)
+admin_site.register(ClusterSubject)
+admin_site.register(ClusterSubjectCategory)
+admin_site.register(Grade)
+# admin_site.register(Application)
+admin_site.register(Basket)
+admin_site.register(Payment)
+
+
+# admin.site.register(Applicant)
+# admin.site.register(Ministry)
+# admin.site.register(MinistryType)
--- PlacementProject/applicant/models.py
+++ PlacementProject/applicant/models.py
@@ -3,33 +3,99 @@ from django.db import models
 
 class Applicant(models.Model):
     Id = models.BigAutoField(primary_key=True)
-    f_name = models.CharField(max_length=40)
-    s_name = models.CharField(max_length=40)
-    l_name = models.CharField(max_length=40)
-    email = models.EmailField()
-    phone = models.PositiveIntegerField()
-    kcse_index = models.PositiveIntegerField()
-    year = models.PositiveIntegerField()
-    birth_cert_no = models.PositiveIntegerField()
+    First_Name = models.CharField(max_length=40)
+    Second_Name = models.CharField(max_length=40)
+    Last_Name = models.CharField(max_length=40)
+    Email = models.EmailField()
+    Phone = models.PositiveIntegerField()
+    KCSE_Index = models.PositiveIntegerField()
+    Year = models.PositiveIntegerField()
+    Birth_Certificate_No = models.PositiveIntegerField()
 
 
 class Institution(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    inst_name = models.TextField(max_length=200)
-    type_id = models.ForeignKey("Ministry", models.SET_NULL,
+    Id = models.BigAutoField(primary_key=True)
+    Institution_Name = models.TextField(max_length=200)
+    Type_Id = models.ForeignKey("MinistryType", models.SET_NULL,
     blank=True, null=True)
-    ministry_id = models.ForeignKey("MinistryType", models.SET_NULL,
+    Ministry_Id = models.ForeignKey("Ministry", models.SET_NULL,
     blank=True, null=True)
 
 
 class Ministry(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    min_name = models.TimeField(max_length=200)
+    Id = models.BigAutoField(primary_key=True)
+    Ministry_Name = models.TimeField(max_length=200)
 
 
 class MinistryType(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    type_name = models.CharField(max_length=25)
+    Id = models.BigAutoField(primary_key=True)
+    Type_Name = models.CharField(max_length=25)
 
+
+class Course(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Course_Name = models.CharField(max_length=200)
+    Cluster_Id = models.ForeignKey("Cluster", models.SET_NULL, 
+    blank=True, null=True)
+    Institution_Id = models.ForeignKey("Institution", models.SET_NULL,
+    null=True, blank=True)
+    Course_Code = models.PositiveIntegerField()
+    Cost = models.PositiveIntegerField()
+    Capacity = models.PositiveIntegerField()
+
+
+class Cluster(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Cluster_Name = models.CharField(max_length=200)
+
+
+class ClusterSubject(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Cluster_Subject_Name = models.CharField(max_length=200)
+    Cluster_Category_Id = models.ForeignKey("ClusterSubjectCategory", models.SET_NULL,
+    blank=True, null=True )
+
+
+class ClusterSubjectCategory(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Cluster_Subject_Category_Name = models.CharField(max_length=200)
+
+
+class Grade(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Grade_Name = models.CharField(max_length=200)
+    Grade_Value = models.PositiveIntegerField()
+
+
+class Basket(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Applicant_Id = models.ForeignKey("Applicant", models.SET_NULL,
+    blank=True, null=True)
+
+# class Application(models.Model):
+#     Id = models.BigAutoField(primary_key=True)
+#     Applicant_Id = models.ForeignKey("Applicant", models.SET_NULL,
+#     null=True, blank=True)
+#     First_Course_Id = models.ForeignKey("Course", models.SET_NULL,
+#     blank=True, null=True)
+#     Second_Course_Id = models.ForeignKey("Course", models.SET_NULL,
+#     blank=True, null=True)
+#     Third_Course_Id = models.ForeignKey("Course", models.SET_NULL, blank=True,
+#     null=True)
+#     Fourth_Course_Id = models.ForeignKey("Course", models.SET_NULL,
+#     blank=True, null=True)
+#     Fifth_Course_Id = models.ForeignKey("Course", models.SET_NULL,
+#     blank=True, null=True)
+#     Sixth_Course_Id = models.ForeignKey("Course", models.SET_NULL, blank=True,
+#     null=True)
+#     Payment_Id = models.ForeignKey("Payment", models.SET_NULL,
+#     blank=True, null=True)
+
+class Payment(models.Model):
+    Id = models.BigAutoField(primary_key=True)
+    Applicant_Id = models.ForeignKey("Applicant", models.SET_NULL,
+    blank=True, null=True)
+    Payment_Id = models.ForeignKey("Payment", models.SET_NULL,
+    blank=True, null=True)
 
 
